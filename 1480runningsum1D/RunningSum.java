6f8e2499be9fa52917b2c631d2092ac9c70d7aed
amresh tripathi <amresh2k@gmail.com>
2024-10-10 22:25:41 +0530

    adding running sum code solution in place, Leetcode 1480

--- 1480runningsum1D/RunningSum.java
+++ 1480runningsum1D/RunningSum.java
@@ -1,11 +1,27 @@
 class RunningSum {
+	
     public int[] runningSum(int[] nums) {
         int[] results = new int[nums.length];
 		results[0] = nums[0];
 		
 		for (int i =0 ; i< nums.length; i++) {
-			results[i] = num[i]+ results[i-1];
+			results[i] = nums[i]+ results[i-1];
 		}
 		return results;
+	
+	// time complexity = O(n)
+	// space complexity = O(1)
+    }    
+	
+	public int[] runningSumInPlace(int[] nums) {
+
+		
+		for (int i =0 ; i< nums.length; i++) {
+			nums[i]+= nums[i-1];
+		}
+		return nums;
     }
+	
+	// time complexity = O(n)
+	// space complexity = O(1)
 }
