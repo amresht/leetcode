64db8fffc0717fabf9281ce053cc4d153e650728
amresh tripathi <amresh2k@gmail.com>
2024-10-13 14:29:06 +0530

    ''

--- 1480runningsum1D/RunningSum.java
+++ 1480runningsum1D/RunningSum.java
@@ -4,7 +4,7 @@ class RunningSum {
         int[] results = new int[nums.length];
 		results[0] = nums[0];
 		
-		for (int i =0 ; i< nums.length; i++) {
+		for (int i =1  ; i< nums.length; i++) {
 			results[i] = nums[i]+ results[i-1];
 		}
 		return results;
@@ -12,6 +12,22 @@ class RunningSum {
 	// time complexity = O(n)
 	// space complexity = O(1)
     }    
+	
+	
+	public int[] runningSum(int[] nums) {
+        int[] arr=new int[nums.length];
+        arr[0]=nums[0];
+		
+        for(int i=1;i<nums.length;i++){
+            int sum=0;
+            for(int j=i;j>=0;j--){
+                sum=sum+nums[j];
+            }
+            arr[i]=sum;
+        }
+        return arr;
+    }	
+	
 	
 	public int[] runningSumInPlace(int[] nums) {
 
